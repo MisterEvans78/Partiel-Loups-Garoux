f213a8ba80a7733cee69f38fbfedbc182ad3fd10
Evans <71824258+MisterEvans78@users.noreply.github.com>
2023-01-11 11:34:06 +0100

    modification javascript

--- js/fct.js
+++ js/fct.js
@@ -1,10 +1,35 @@
-var mdp = document.getElementById("mdp").value;
-var cmdp = document.getElementById("Cmdp").value;
+
+var div_warning = document.getElementById("warning");
+
 function verifPassword() {
+    var mdp = document.getElementById("mdpIns").value;
+    var cmdp = document.getElementById("CmdpIns").value;
+    var buV = document.getElementById("IscBuValidate");
+    console.log(buV)
+    IscBuValidate
     if (mdp != cmdp) {
-        console.log("Les mots de passe ne correspondent pas");
+        div_warning.innerHTML = "<p style='color:red;'>Les mots de passe ne correspondent pas</p>";
+        $("#IscBuValidate").attr("disabled", 'disabled');
+
+    } else {
+       
+        div_warning.innerHTML = "<p style='color:green;'>Les mots de passe correspondent</p>";
+        $("#IscBuValidate").removeAttr("disabled");
+        verifChampsRempInc()
     }
-    else {
-        console.log("Les mots de passe correspondent");
+}
+
+function verifChampsRempInc(){
+    var mdp = document.getElementById("mdpIns").value.length;
+    var cmdp = document.getElementById("CmdpIns").value.length;
+    var pseudo = document.getElementById("pseudoIns").value.length;
+    var mail = document.getElementById("mailIns").value.length;
+    if(mdp!=0 && cmdp!=0 && pseudo!=0 && mail!=0){
+        $("#IscBuValidate").removeAttr("disabled");
+        document.getElementById("warningBis").innerHTML=""
+    }else{
+        $("#IscBuValidate").attr("disabled", 'disabled')
+        document.getElementById("warningBis").innerHTML="<p style='color:red;'>veuillez renseigner tous les champs obligatoires</p>"
+        
     }
 }
